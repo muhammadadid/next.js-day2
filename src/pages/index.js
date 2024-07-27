b96d37219bc77c97688b26ef4d1c1b4cf5c6bb5f
muhammadadid <muhammadadid20@gmail.com>
2024-07-27 15:41:38 +0700

    add Actifity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,32 @@
+import Activity from "@/components/Activity";
+import BackgroundSlider from "@/components/BackgroundSlider";
+import Blog from "@/components/Blog";
+import Category from "@/components/Category";
+import Footer from "@/components/Footer";
+import PromoCard from "@/components/PromoCard"; 
 
-import axios from "axios";
-import { useRouter } from "next/router";
-export async function getServerSideProps(contex) {
-  const res = await axios.get(
-    `https://jsonplaceholder.typicode.com/posts?_page=${contex.query.page}&_limit=20`
-  );
-  const data = res.data;
-  console.log(res);
-  return { props: { postDetail: data } };
-}
-export default function PostDetail({ postDetail }) {
-  const router = useRouter();
-  const page = Number(router.query.page || 1);
+
+const Home = () => {
+  
   return (
-    <div>
-      <ul className="list-disc list-inside">
-        {postDetail.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-      <h1>{postDetail.title}</h1>
-      <button
-        className="p-2 bg-blue-800 rounded"
-        onClick={() => router.push(`?page=${page + 1}`)}
-      >
-        Next
-      </button>
-      <button
-        className="p-2 bg-red-800 rounded"
-      >
-        <a href={`/Promo`}>Prev</a>
-      </button>
+    <div className="w-full relative bg-white overflow-hidden flex flex-col items-start justify-start gap-[134px] leading-[normal] tracking-[normal] mq800:gap-[67px] mq450:gap-[33px]">
+      {/* <section className="self-stretch flex flex-row items-start justify-start pt-0 px-0 pb-1.5 box-border max-w-full text-left text-[84px] text-white font-playfair-display"> */}
+        <BackgroundSlider />
+      {/* </section> */}
+      <section className="self-stretch flex flex-row items-start justify-start pt-0 px-8 pb-[148px] box-border max-w-full text-left text-45xl text-gray font-playfair-display mq800:pb-[62px] mq800:box-border mq1125:pb-24 mq1125:box-border">
+        <Category Category={Category}/>
+      </section>
+      <section className="self-stretch flex flex-row items-start justify-start pt-0 px-8 pb-[156px] box-border max-w-full text-left text-45xl text-gray font-playfair-display mq800:pb-[66px] mq800:box-border mq1125:pb-[101px] mq1125:box-border">
+        <PromoCard />
+      </section>
+      <section className="self-stretch flex flex-row items-start justify-start pt-0 px-8 pb-[148px] box-border max-w-full text-left text-45xl text-gray font-playfair-display mq800:pb-[62px] mq800:box-border mq1350:pb-24 mq1350:box-border">
+        <Blog />
+      </section>
+      <Activity />
+      <section className="flex flex-col items-start self-stretch justify-start max-w-full text-left text-45xl text-slategray font-playfair-display">
+        <Footer />
+      </section>
     </div>
   );
-}
+};
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,104 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        white: "#fff",
+        gray: {
+          "100": "#fffef9",
+          "200": "#8d8d8d",
+          "300": "#808080",
+          "400": "#172432",
+          "500": "#141b34",
+        },
+        whitesmoke: "#f5f6f7",
+        slategray: "#767e86",
+        khaki: {
+          "100": "#d9ed82",
+          "200": "#bfd469",
+          "300": "rgba(191, 212, 105, 0.52)",
+          "400": "rgba(217, 237, 130, 0.52)",
+          "500": "rgba(191, 212, 105, 0.49)",
+          "600": "rgba(216, 236, 129, 0.49)",
+        },
+        greenyellow: "#ccf32f",
+        yellowgreen: {
+          "100": "#b3d914",
+          "200": "#aec742",
+        },
+        black: "#000",
+        coral: "#ff7757",
+        darkslateblue: "#131e46",
+        midnightblue: "rgba(4, 16, 59, 0.94)",
+        darkgray: {
+          "100": "#adadad",
+          "200": "#ababab",
+        },
+        maroon: "#611502",
+        dodgerblue: "#4285f4",
+        darkblue: "#000088",
+        dark: "#222",
+        darkgoldenrod: "#b87514",
+        burlywood: "#ecbc76",
+      },
+      spacing: {},
+      fontFamily: {
+        rubik: "Rubik",
+        "playfair-display": "'Playfair Display'",
+        mulish: "Mulish",
+        poppins: "Poppins",
+        "dm-sans": "'DM Sans'",
+      },
+      borderRadius: {
+        "81xl": "100px",
+        "7xl": "26px",
+        "21xl": "40px",
+        "4xs": "9px",
+        "3xs": "10px",
+        "22xl-5": "41.5px",
+      },
+    },
+    fontSize: {
+      "5xl": "24px",
+      lgi: "19px",
+      lg: "18px",
+      sm: "14px",
+      "45xl": "64px",
+      "32xl": "51px",
+      "19xl": "38px",
+      "9xl": "28px",
+      "3xl": "22px",
+      "13xl": "32px",
+      xl: "20px",
+      base: "16px",
+      "21xl": "40px",
+      smi: "13px",
+      "36xl": "55px",
+      "14xl": "33px",
+      "25xl": "44px",
+      inherit: "inherit",
+    },
+    screens: {
+      mq1750 : {
+        raw: "screen and (max-width: 1750px)",
+      },
+      mq1350: {
+        raw: "screen and (max-width: 1350px)",
+      },
+      mq1125: {
+        raw: "screen and (max-width: 1125px)",
+      },
+      mq800: {
+        raw: "screen and (max-width: 800px)",
+      },
+      mq450: {
+        raw: "screen and (max-width: 450px)",
+      },
+      mq375: {
+        raw: "screen and (max-width: 375px)",
       },
     },
   },
-  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 };
